5559c5b1a6b3632f7f4246ff9d44da0846c6d124
evelynda1985 <evelyn.sanchez.agredo@gmail.com>
2020-12-23 21:41:00 -0500

    Initial commit

---
+++ src/com/evelynda1985/Loops1.java
@@ -0,0 +1,13 @@
+package com.evelynda1985;
+
+public class Loops1 {
+
+    public static void main(String[] args) {
+
+        //Replace 2 by N for hackerRank!
+        int input = 2;
+        for (int i = 1; i <=10; i++){
+            System.out.println(input + " x " + i + " = " + input * i);
+        }
+    }
+}
